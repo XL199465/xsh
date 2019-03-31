a5a08911f6810e8a4eb83bd8f66241141cbfe5f2
liujunjie <2631471554@qq.com>
2019-03-31 14:59:00 +0800

    修改了web-user里的TestCcontroller文件  --刘俊杰

--- parent-337/pinyougou-web-user/src/main/java/cn/itcast/core/controller/TestController.java
+++ parent-337/pinyougou-web-user/src/main/java/cn/itcast/core/controller/TestController.java
@@ -3,5 +3,6 @@ package cn.itcast.core.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("1111111111111111111");
+        System.out.println("2222222222222222222");
     }
 }
